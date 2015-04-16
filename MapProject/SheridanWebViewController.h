a72d09b680cfb12bd376c0b1727656226ba88cfd
Gordon Cox <cox.gordon@live.ca>
2015-04-15 20:02:08 -0400

    Even MOAR files, yeah! database added and a couple more view controllers

---
+++ MapProject/SheridanWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  SheridanWebViewController.h
+//  MapProject
+//
+//  Created by xcode on 2015-04-15.
+//  Copyright (c) 2015 Alex Estrop. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SheridanWebViewController : ViewController
+
+@end
