250a7025a89c72cd553299b5145ab48ace089ccc
Gordon Cox <cox.gordon@live.ca>
2015-04-15 20:47:48 -0400

    Added Classroom propertie

--- MapProject/Classroom.h
+++ MapProject/Classroom.h
@@ -2,9 +2,7 @@
 //  Classroom.h
 //  MapProject
 //
-//  Created by xcode on 2015-04-15.
-//  Copyright (c) 2015 Alex Estrop. All rights reserved.
-//
+//  Created by Gordon Cox
 
 #import <Foundation/Foundation.h>
 
