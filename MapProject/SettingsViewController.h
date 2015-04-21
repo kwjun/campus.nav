9e8947ceef19964b20d64b6aaee76d2abd9fd8d7
KwangJun Ko <Jun@Juns-iMac.local>
2015-04-21 00:17:19 -0400

    SettingVC and layout fixed

---
+++ MapProject/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MapProject
+//
+//  Created by KwangJun Ko on 2015-04-21.
+//  Copyright (c) 2015 Alex Estrop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
