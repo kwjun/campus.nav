a1f71da9769b6bda7e0845bb56616c4ac6427898
IOS Project <xcode@admins-MacBook-Pro.local>
2015-04-20 20:41:54 -0400

    my shit

---
+++ MapProject/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  MapProject
+//
+//  Created by xcode on 2015-04-20.
+//  Copyright (c) 2015 Alex Estrop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
---
+++ MapProject/EventViewController.m
@@ -0,0 +1,37 @@
+//
+//  EventViewController.m
+//  MapProject
+//
+//  Created by xcode on 2015-04-20.
+//  Copyright (c) 2015 Alex Estrop. All rights reserved.
+//
+
+#import "EventViewController.h"
+
+@interface EventViewController ()
+
+@end
+
+@implementation EventViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
